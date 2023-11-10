0339647c8c90180d598cd4f66e38b3b62f0b5da8
vladstojna <vladstojna@gmail.com>
2023-11-10 17:19:09 +0300

    change file tree to oil.nvim

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,33 +1,5 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  opts = {
-    window = {
-      position = "float",
-      mappings = {
-        ["<space>"] = "none",
-      },
-    },
-    filesystem = {
-      filtered_items = {
-        visible = false,
-        hide_dotfiles = true,
-        hide_gitignored = true,
-      },
-      bind_to_cwd = false,
-      follow_current_file = {
-        enabled = true,
-      },
-      hijack_netrw_behavior = "open_current",
-    },
-  },
-  config = function(_, opts)
-    vim.g.neo_tree_remove_legacy_commands = 1
-    require("neo-tree").setup(opts)
-  end,
+  'stevearc/oil.nvim',
+  opts = {},
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
 }

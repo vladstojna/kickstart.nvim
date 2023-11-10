9a709ec320e31132166b9a99949c65ca47556de5
vladstojna <vladstojna@gmail.com>
2023-11-10 19:00:25 +0300

    reformat remap and add hlsearch toggle

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -1,36 +1,36 @@
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-s>", "<C-u>zz")
-vim.keymap.set("n", "{", "{zz")
-vim.keymap.set("n", "}", "}zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-s>', '<C-u>zz')
+vim.keymap.set('n', '{', '{zz')
+vim.keymap.set('n', '}', '}zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
 
-vim.keymap.set("n", "<Home>", "_")
-vim.keymap.set("n", "<End>", "$")
+vim.keymap.set('n', '<Home>', '_')
+vim.keymap.set('n', '<End>', '$')
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set('x', '<leader>p', [["_dP]])
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set('i', '<C-c>', '<Esc>')
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format,
-    { desc = "LSP: format buffer" })
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'LSP: format buffer' })
 
-vim.keymap.set("n", "<leader>ru",
-    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "[R]eplace word [U]nder cursor" })
-vim.keymap.set("x", "<leader>r",
-    [["hy:%s/<C-r>h/<C-r>h/gI<Left><Left><Left>]],
-    { desc = "[R]eplace selection" })
+vim.keymap.set('n', '<leader>ru', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = '[R]eplace word [U]nder cursor' })
+vim.keymap.set('x', '<leader>r', [["hy:%s/<C-r>h/<C-r>h/gI<Left><Left><Left>]], { desc = '[R]eplace selection' })
 
-vim.keymap.set("n", "S", ":w<cr>")
-vim.keymap.set("n", "Q", ":q<cr>")
+vim.keymap.set('n', 'S', ':w<cr>')
+vim.keymap.set('n', 'Q', ':q<cr>')
+
+vim.keymap.set('n', '<leader>+', function()
+    vim.o.hlsearch = not vim.o.hlsearch
+end, { desc = 'Toggle highlight during search' })

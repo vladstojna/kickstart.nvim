d8ffa9841196c71cec2b31de8daaa453503dd72a
vladstojna <vladstojna@gmail.com>
2023-11-10 17:20:49 +0300

    remove bufferline

--- after/plugin/bufferline.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps
-
-local opts = { silent = true, noremap = true }
-
-vim.keymap.set('n', '<leader>bp', ':BufferLineTogglePin<CR>', opts)
-vim.keymap.set('n', '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>',
-    { silent = true, noremap = true, desc = 'Delete non-pinned buffers' })

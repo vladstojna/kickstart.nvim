cbdbc4924d0d795f02e1dce3f6f29e040f8678c8
vladstojna <29868090+vladstojna@users.noreply.github.com>
2023-12-08 20:22:42 +0300

    change oil and Home keymaps

--- after/plugin/filetree.lua
+++ after/plugin/filetree.lua
@@ -1,2 +1,3 @@
-vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = 'Explore parent directory of current buffer', silent = true })
-vim.keymap.set('n', '<leader>E', '<cmd>Oil .<cr>', { desc = '[E]xplore root directory', silent = true })
+vim.keymap.set('n', '<leader>-', '<cmd>Oil<cr>',
+    { noremap = true, desc = 'Explore parent directory of current buffer', silent = true })
+vim.keymap.set('n', '<leader>E', '<cmd>Oil .<cr>', { noremap = true, desc = '[E]xplore root directory', silent = true })

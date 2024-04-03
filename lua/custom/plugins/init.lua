05d86673aeca4611fb6a72b8ef94cd0f4d713a30
vladstojna <vladstojna@gmail.com>
2024-04-03 21:02:27 +0300

    suppress fidget on insert

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -204,6 +204,9 @@ return {
   {
     'j-hui/fidget.nvim',
     opts = {
+      progress = {
+        suppress_on_insert = true,
+      },
       notification = {
         window = {
           winblend = 0,

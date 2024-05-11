20b1153b884c43879442c7134aff626d1b298e18
vladstojna <29868090+vladstojna@users.noreply.github.com>
2024-05-11 19:01:46 +0300

    remove some redundant plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -88,72 +88,15 @@ return {
     end,
   },
   {
-    'folke/todo-comments.nvim',
-    cmd = { 'TodoTrouble', 'TodoTelescope' },
-    config = true,
-    keys = {
-      {
-        ']/',
-        function()
-          require('todo-comments').jump_next()
-        end,
-        desc = 'Next todo comment',
-      },
-      {
-        '[/',
-        function()
-          require('todo-comments').jump_prev()
-        end,
-        desc = 'Previous todo comment',
-      },
-      { '<leader>xt', '<cmd>TodoTrouble<cr>',                           desc = 'Todo (Trouble)' },
-      { '<leader>xT', '<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>',   desc = 'Todo/Fix/Fixme (Trouble)' },
-      { '<leader>st', '<cmd>TodoTelescope<cr>',                         desc = 'Todo' },
-      { '<leader>sT', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', desc = 'Todo/Fix/Fixme' },
-    },
-  },
-  {
-    'echasnovski/mini.pairs',
-    version = false,
-    config = function()
-      require('mini.pairs').setup()
-    end,
-  },
-  {
-    'echasnovski/mini.trailspace',
-    version = false,
-    config = function()
-      require('mini.trailspace').setup()
-    end,
-  },
-  {
-    'echasnovski/mini.surround',
-    version = false,
-    opts = {
-      mappings = {
-        add = 'gsa',            -- Add surrounding in Normal and Visual modes
-        delete = 'gsd',         -- Delete surrounding
-        find = 'gsf',           -- Find surrounding (to the right)
-        find_left = 'gsF',      -- Find surrounding (to the left)
-        highlight = 'gsh',      -- Highlight surrounding
-        replace = 'gsr',        -- Replace surrounding
-        update_n_lines = 'gsn', -- Update `n_lines`
-      },
-    },
-    config = function(_, opts)
-      require('mini.surround').setup(opts)
-    end,
-  },
-  {
     'rmagatti/goto-preview',
     config = function()
       require('goto-preview').setup {
-        width = 100,             -- Width of the floating window
-        height = 20,             -- Height of the floating window
+        width = 100, -- Width of the floating window
+        height = 20, -- Height of the floating window
         default_mappings = true, -- Bind default mappings
-        debug = false,           -- Print debug information
-        opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
-        post_open_hook = nil,    -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        debug = false, -- Print debug information
+        opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
       }
     end,
   },

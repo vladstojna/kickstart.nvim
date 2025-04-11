6cfc508fc7487f1779b21286a68717e3f976634c
vladstojna <29868090+vladstojna@users.noreply.github.com>
2025-04-11 16:39:09 +0100

    fix tokyonight transparent key

--- init.lua
+++ init.lua
@@ -883,8 +883,8 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
-          transparent = true,
           comments = { italic = false }, -- Disable italics in comments
         },
       }

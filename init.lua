449e2fbffc98535a0e759086c3ec6de93e66ebf2
vladstojna <29868090+vladstojna@users.noreply.github.com>
2025-04-11 17:12:39 +0100

    add C-s to accept completion in blink

--- init.lua
+++ init.lua
@@ -834,6 +834,8 @@ require('lazy').setup({
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
+        ['<C-s>'] = { 'select_and_accept' },
+
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },

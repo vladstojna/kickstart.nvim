b229410e51222ddd5ac4fa4b02d534342959a09e
vladstojna <29868090+vladstojna@users.noreply.github.com>
2025-08-05 20:26:31 +0100

    add render-markdown.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -28,6 +28,15 @@ return {
     },
   },
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      completions = { blink = { enabled = true } },
+    },
+  },
+  {
     'rmagatti/auto-session',
     config = function()
       require('auto-session').setup {

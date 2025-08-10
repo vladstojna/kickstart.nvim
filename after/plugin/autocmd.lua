61750fa9ccf2b7f610274194ad07055ab9ec1be4
vladstojna <29868090+vladstojna@users.noreply.github.com>
2025-08-10 14:14:49 +0100

    Add copilot chat

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -7,3 +7,12 @@ vim.api.nvim_create_autocmd('SessionLoadPost', {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = 'copilot-*',
+  callback = function()
+    vim.opt_local.relativenumber = false
+    vim.opt_local.number = false
+    vim.opt_local.conceallevel = 0
+  end,
+})

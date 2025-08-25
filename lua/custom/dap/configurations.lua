abfdbcc550d9ce54ffca97fd26a27ddb99966810
vladstojna <29868090+vladstojna@users.noreply.github.com>
2025-08-25 22:45:19 +0100

    Rewrite dap/configurations and add more configs

--- lua/custom/dap/configurations.lua
+++ lua/custom/dap/configurations.lua
@@ -1,52 +1,13 @@
 local dap = require 'dap'
 
-local function insert_configs(tab, debugger_name)
-  -- General configurations,
-  -- adapted from: https://blog.cryptomilk.org/2024/01/02/neovim-dap-and-gdb-14-1/
-  table.insert(tab, {
-    name = 'Run executable (' .. debugger_name .. ')',
-    type = debugger_name,
-    request = 'launch',
-    -- This requires special handling of 'run_last', see
-    -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
-    program = function()
-      local path = vim.fn.input {
-        prompt = 'Path to executable: ',
-        default = vim.fn.getcwd() .. '/',
-        completion = 'file',
-      }
+local function executable_input()
+  local path = vim.fn.input {
+    prompt = 'Path to executable: ',
+    default = vim.fn.getcwd() .. '/',
+    completion = 'file',
+  }
 
-      return (path and path ~= '') and path or dap.ABORT
-    end,
-  })
-  table.insert(tab, {
-    name = 'Run executable with arguments (' .. debugger_name .. ')',
-    type = debugger_name,
-    request = 'launch',
-    -- This requires special handling of 'run_last', see
-    -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
-    program = function()
-      local path = vim.fn.input {
-        prompt = 'Path to executable: ',
-        default = vim.fn.getcwd() .. '/',
-        completion = 'file',
-      }
-
-      return (path and path ~= '') and path or dap.ABORT
-    end,
-    args = function()
-      local args_str = vim.fn.input {
-        prompt = 'Arguments: ',
-      }
-      return vim.split(args_str, ' +')
-    end,
-  })
-  table.insert(tab, {
-    name = 'Attach to process (' .. debugger_name .. ')',
-    type = debugger_name,
-    request = 'attach',
-    processId = require('dap.utils').pick_process,
-  })
+  return (path and path ~= '') and path or dap.ABORT
 end
 
 local function gdbserver_attach(tab, adapter_name, libs_type)
@@ -61,9 +22,67 @@ local function gdbserver_attach(tab, adapter_name, libs_type)
   })
 end
 
+local launch = function(adapter)
+  return {
+    name = 'Run executable (' .. adapter .. ')',
+    type = adapter,
+    request = 'launch',
+    -- This requires special handling of 'run_last', see
+    -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+    program = executable_input,
+    cwd = '${workspaceFolder}',
+  }
+end
+
+local launch_with_args = function(adapter)
+  return {
+    name = 'Run executable with arguments (' .. adapter .. ')',
+    type = adapter,
+    request = 'launch',
+    -- This requires special handling of 'run_last', see
+    -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+    program = executable_input,
+    cwd = '${workspaceFolder}',
+    args = function()
+      local args_str = vim.fn.input {
+        prompt = 'Arguments: ',
+      }
+      return vim.split(args_str, ' +')
+    end,
+  }
+end
+
+local attach = function(adapter)
+  return {
+    name = 'Attach to process (' .. adapter .. ')',
+    type = adapter,
+    request = 'attach',
+    processId = require('dap.utils').pick_process,
+    cwd = '${workspaceFolder}',
+  }
+end
+
 local M = {}
-insert_configs(M, 'gdb')
-insert_configs(M, 'lldb')
+
+for _, what in ipairs { launch, launch_with_args, attach } do
+  table.insert(
+    M,
+    vim.tbl_extend('error', {
+      setupCommands = {
+        {
+          text = '-enable-pretty-printing',
+          description = 'enable pretty printing',
+          ignoreFailures = false,
+        },
+      },
+    }, what 'cppdbg')
+  )
+  table.insert(M, what 'codelldb')
+  table.insert(M, what 'gdb')
+  table.insert(M, what 'lldb')
+end
+
 gdbserver_attach(M, 'gdb_localsysroot', 'local libs')
 gdbserver_attach(M, 'gdb', 'remote libs')
+
 return M

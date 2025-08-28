846893257b0d6d5435e2fbcfa34e042a1269ac9e
vladstojna <29868090+vladstojna@users.noreply.github.com>
2025-08-28 18:56:23 +0100

    Remove unused keymaps and change diagnostic jumps

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -1,6 +1,5 @@
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', '//', 'y/\\V<C-R>"<CR>N')
 
 vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -24,14 +23,12 @@ vim.keymap.set('n', '<leader>ru', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>
 vim.keymap.set('x', '<leader>r', [["hy:%s/<C-r>h/<C-r>h/gI<Left><Left><Left>]], { desc = '[R]eplace selection' })
 
 vim.keymap.set('n', '<leader>w', vim.cmd.w, { silent = true, noremap = true, desc = 'Save buffer' })
-vim.keymap.set('n', '<leader>q', vim.cmd.q, { silent = true, noremap = true, desc = 'Quit' })
-vim.keymap.set('n', '<leader>qq', vim.cmd.qa, { silent = true, noremap = true, desc = 'Quit all' })
 
-vim.keymap.set('n', '<C-k>', function()
-  vim.diagnostic.jump { count = -1, float = true }
+vim.keymap.set('n', '[3', function()
+  vim.diagnostic.jump { count = -1 }
 end, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', '<C-j>', function()
-  vim.diagnostic.jump { count = 1, float = true }
+vim.keymap.set('n', ']3', function()
+  vim.diagnostic.jump { count = 1 }
 end, { desc = 'Go to next diagnostic message' })
 
 vim.keymap.set('n', '<leader><Tab>', ':b#<cr>', { silent = true, noremap = true, desc = 'Toggle between last two buffers' })

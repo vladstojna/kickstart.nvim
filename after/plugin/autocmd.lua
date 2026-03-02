cf33b1f3f998b022e44caaa5b91a74aaec2a8448
vladstojna <29868090+vladstojna@users.noreply.github.com>
2026-03-02 17:06:41 +0000

    Dynamic cmdheight when entering/exiting cmdline

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -7,3 +7,25 @@ vim.api.nvim_create_autocmd('SessionLoadPost', {
     end
   end,
 })
+
+local cmdGrp = vim.api.nvim_create_augroup('cmdline_height', { clear = true })
+local function set_cmdheight(val)
+  if vim.opt.cmdheight:get() ~= val then
+    vim.opt.cmdheight = val
+    vim.cmd.redrawstatus()
+  end
+end
+
+vim.api.nvim_create_autocmd('CmdlineEnter', {
+  group = cmdGrp,
+  callback = function()
+    set_cmdheight(1)
+  end,
+})
+
+vim.api.nvim_create_autocmd('CmdlineLeave', {
+  group = cmdGrp,
+  callback = function()
+    set_cmdheight(0)
+  end,
+})

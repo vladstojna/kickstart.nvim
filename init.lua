dfaf266ae6056fc407fa8deb1b903bab0fb097ec
vladstojna <29868090+vladstojna@users.noreply.github.com>
2026-03-05 18:05:26 +0000

    Disable diagnostic virtual text

--- init.lua
+++ init.lua
@@ -180,7 +180,7 @@ vim.diagnostic.config {
   underline = { severity = { min = vim.diagnostic.severity.WARN } },
 
   -- Can switch between these as you prefer
-  virtual_text = true, -- Text shows up at the end of the line
+  virtual_text = false, -- Text shows up at the end of the line
   virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
   -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`

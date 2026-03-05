19ac03f511c4b8be78952f8a08c2954d1faecbbc
vladstojna <29868090+vladstojna@users.noreply.github.com>
2026-03-05 17:31:24 +0000

    Change keymap to search help and add keymap to search history

--- init.lua
+++ init.lua
@@ -422,7 +422,8 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sH', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sh', builtin.command_history, { desc = '[S]earch [H]istory: commands' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
